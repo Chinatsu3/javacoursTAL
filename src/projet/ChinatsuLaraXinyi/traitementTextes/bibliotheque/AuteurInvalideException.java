b1a76157f3dbe034f24518b84cbd6505e03c9208
Chinatsu3 <chinatsu07.k@gmail.com>
2021-01-06 15:05:24 +0100

    ajout AuteurInvalideException

---
+++ src/projet/ChinatsuLaraXinyi/traitementTextes/bibliotheque/AuteurInvalideException.java
@@ -0,0 +1,11 @@
+package projet.ChinatsuLaraXinyi.traitementTextes.bibliotheque;
+
+public class AuteurInvalideException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public AuteurInvalideException(String message) {
+		super(message);
+	}
+
+}
